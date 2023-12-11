7b07a57914da6990a529e59cf7ba4ea776222e99
Gulchohra Abdurahman <ab.gulchohra@gmail.com>
2023-12-11 12:05:14 +0100

    request/response models added

---
+++ Tasks.API/Contracts/V1/Tasks.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Extensions;
+using Tasks.API.Domain;
+
+namespace Tasks.API.Contracts.V1
+{
+    public class Tasks
+    {
+        #region Requests
+        public record CreateTaskRequest(string title, string description, DateTime dueDate)
+        {
+            public static explicit operator TaskItem(CreateTaskRequest request)
+            {
+                return new TaskItem(
+                    request.title,
+                    request.description,
+                    request.dueDate);
+            }
+
+        }
+        public record UpdateTaskRequest(string? title, string? description, string? status);
+        #endregion
+
+        #region Responses
+        public record GetTaskResponse(Guid id, string title, string description, DateTime dueDate, Status status)
+        {
+            public static explicit operator GetTaskResponse(TaskItem task)
+            {
+                return new GetTaskResponse(task.Id, task.Title, task.Description, task.DueDate, task.Status);
+            }
+        }
+        public record CreateTaskResponse(Guid id ,string title, string description, DateTime dueDate, Status status)
+        {
+            public static explicit operator CreateTaskResponse(TaskItem task)
+            {
+                return new CreateTaskResponse(task.Id, task.Title, task.Description, task.DueDate, task.Status);
+            }
+        }
+        #endregion
+    }
+}
