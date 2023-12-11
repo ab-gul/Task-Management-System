1cd7031b0870c3481637dc1bf49809e8c5639e39
Gulchohra Abdurahman <ab.gulchohra@gmail.com>
2023-12-11 12:04:22 +0100

    Endpoints Added

---
+++ Tasks.API/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,21 @@
+﻿namespace Tasks.API.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        private const string Root = "api";
+        private const string Version = "v1";
+        private const string Base = Root + "/" + Version;
+
+        public static class Tasks
+        {
+            public const string GetAll = Base + "/tasks";
+            public const string Get = Base + "/tasks/{id:Guid}";
+            public const string Create = Base + "/tasks";
+            public const string Update = Base + "/tasks/{id:Guid}";
+            public const string Delete = Base + "/tasks/{id:Guid}";
+        }
+
+
+
+    }
+}
