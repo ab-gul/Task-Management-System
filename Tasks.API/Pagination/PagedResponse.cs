999e9b3aeff0f8ccc92f6252fc95b5df00f94797
Gulchohra Abdurahman <ab.gulchohra@gmail.com>
2023-12-13 01:06:15 +0100

    renamed PagedResponse, removed public ctor, Uris

---
+++ Tasks.API/Pagination/PagedResponse.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tasks.API.Pagination
+{
+    public class PagedResponse<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int PageNumber { get; }
+        public int PageSize { get; }
+        public int TotalCount { get; }
+
+
+        private PagedResponse(IEnumerable<T> items,int pageNumber, int pageSize, int totalCount)
+        {
+            this.Items = items;
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+            this.TotalCount = totalCount;
+        }
+
+
+        public static PagedResponse<T> Create(IEnumerable<T> items, int pageNumber, int pageSize, int totalCount)
+        {
+            var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+
+            return new PagedResponse<T>(items, pageNumber, pageSize, totalCount);
+        }
+    }
+}
