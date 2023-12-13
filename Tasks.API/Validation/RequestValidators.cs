27680a01b4fd497f910ace4632a863ccf9778358
Gulchohra Abdurahman <ab.gulchohra@gmail.com>
2023-12-13 01:08:15 +0100

    removed unnecessary validations

--- Tasks.API/Validation/RequestValidators.cs
+++ Tasks.API/Validation/RequestValidators.cs
@@ -5,52 +5,27 @@ namespace Tasks.API.Validation
 {
     public class Validators
     {
-        private static bool IsValidGuid(Guid unValidatedGuid)
-        {
-            if (Guid.TryParse(unValidatedGuid.ToString(), out Guid validatedGuid))
-            {
-                return validatedGuid != Guid.Empty;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private static bool IsValidGuid(Guid? unValidatedGuid)
-        {
-            if (Guid.TryParse(unValidatedGuid.ToString(), out Guid validatedGuid))
-            {
-                return validatedGuid != Guid.Empty;
-
-            }
-            else
-            {
-                return false;
-            }
-        }
-
         public sealed class CreateTaskRequestValidator : AbstractValidator<CreateTaskRequest>
         {
             public CreateTaskRequestValidator()
             {
                 RuleFor(c => c.description)
                     .NotNull()
-                    .WithMessage(" \'Description\' section can not be null")
                     .NotEmpty()
-                    .WithMessage(" \'Description\' section can not be empty or whitespace");
+                    .WithMessage(" \'Description\' section can not be null, empty or whitespace");
 
                 RuleFor(c => c.title)
                     .NotNull()
-                    .WithMessage(" \'Title\' section can not be null")
                     .NotEmpty()
-                    .WithMessage(" \'Title\' section can not be empty or whitespace");
+                    .WithMessage(" \'Title\' section can not be null, empty or whitespace");
+
                 RuleFor(c => c.dueDate)
+                    .Cascade(CascadeMode.Stop)
                     .NotNull()
-                    .WithMessage(" \'Due Date\' section can not be null")
                     .NotEmpty()
-                    .WithMessage(" \'Due Date\' section can not be empty or whitespace")
-                    .Must(c => c.ToUniversalTime() > DateTime.UtcNow).WithMessage("\'Due Date\' section can not be past date");
+                    .WithMessage(" \'Due Date\' section can not be null, empty or whitespace")
+                    .Must(c => c > DateTime.Now)
+                    .WithMessage("\'Due Date\' section can not be past date");
 
             }
         }
@@ -61,11 +36,13 @@ namespace Tasks.API.Validation
             {
 
                 RuleFor(c => c.title)
-                    .NotEmpty().When(d => d.title != null)
+                    .NotEmpty()
+                    .When(d => d.title != null)
                     .WithMessage("\'Title\' section can not be empty or whitespace");
 
                 RuleFor(c => c.description)
-                    .NotEmpty().When(d => d.description != null)
+                    .NotEmpty()
+                    .When(d => d.description != null)
                     .WithMessage("\'Description\' section can not be empty or whitespace");
             }
         }
